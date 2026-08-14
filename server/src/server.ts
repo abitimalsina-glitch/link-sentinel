8f3d1a235c6a46b4a07be6c00015bda250074760
abitimalsina-glitch <abitimalsina4@gmail.com>
2026-08-14 04:23:41 -0700

    document: add README.md

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,6 @@ import {type Request, type Response} from 'express';
 
 const port = Number(process.env.PORT) || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Server is running" });
-});
-
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
